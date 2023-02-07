bfbfe792af4fed0845c54506c5e874cf122214bd
luke_sch02 <lukas.schmidt@gmx.ch>
2023-02-07 16:01:36 +0100

    Glass Cuboid: adressed recommended changes

--- main/src/init/kelvin_helmholtz_init.hpp
+++ main/src/init/kelvin_helmholtz_init.hpp
@@ -36,6 +36,7 @@
 #include "cstone/primitives/gather.hpp"
 #include "io/mpi_file_utils.hpp"
 #include "isim_init.hpp"
+#include "utils.hpp"
 
 #include "grid.hpp"
 
@@ -101,52 +102,6 @@ void initKelvinHelmholtzFields(Dataset& d, const std::map<std::string, double>& 
     }
 }
 
-template<class T, class Dataset>
-auto makeHalfDenseTemplate(std::vector<T> x, std::vector<T> y, std::vector<T> z, size_t blockSize)
-{
-    using KeyType = typename Dataset::KeyType;
-
-    std::vector<T> xHalf, yHalf, zHalf;
-    xHalf.reserve(blockSize);
-    yHalf.reserve(blockSize);
-    zHalf.reserve(blockSize);
-    cstone::Box<T> templateBox(0, 1, 0, 1, 0, 1);
-
-    std::vector<KeyType> codes(blockSize);
-    computeSfcKeys(x.data(), y.data(), z.data(), cstone::sfcKindPointer(codes.data()), blockSize, templateBox);
-
-    std::vector<cstone::LocalIndex> sfcOrder(blockSize);
-    std::iota(begin(sfcOrder), end(sfcOrder), cstone::LocalIndex(0));
-    cstone::sort_by_key(begin(codes), end(codes), begin(sfcOrder));
-
-    std::vector<T> buffer(blockSize);
-    cstone::gather<cstone::LocalIndex>(sfcOrder, x.data(), buffer.data());
-    std::swap(x, buffer);
-    cstone::gather<cstone::LocalIndex>(sfcOrder, y.data(), buffer.data());
-    std::swap(y, buffer);
-    cstone::gather<cstone::LocalIndex>(sfcOrder, z.data(), buffer.data());
-    std::swap(z, buffer);
-
-    for (size_t i = 0; i < blockSize; i += 2)
-    {
-        xHalf.push_back(x[i]);
-        yHalf.push_back(y[i]);
-        zHalf.push_back(z[i]);
-    }
-
-    xHalf.shrink_to_fit();
-    yHalf.shrink_to_fit();
-    zHalf.shrink_to_fit();
-
-    return std::make_tuple(xHalf, yHalf, zHalf);
-}
-
-/*!
- * @brief assembles the global Kelvin-Helmholtz initial conditions
- *
- * @params x_HD, y_HD, z_HD:     x, y and z coordinate vector of the high density template
- * @params x_LD, y_LD, z_HD:     x, y and z coordinate vector of the low density template
- */
 std::map<std::string, double> KelvinHelmholtzConstants()
 {
     return {{"rhoInt", 2.},     {"rhoExt", 1.},          {"vxExt", 0.5}, {"vxInt", -0.5},
@@ -183,7 +138,7 @@ public:
                                  cstone::BoundaryType::periodic);
         auto [keyStart, keyEnd] = partitionRange(cstone::nodeRange<KeyType>(0), rank, numRanks);
 
-        auto [xHalf, yHalf, zHalf] = makeHalfDenseTemplate<T, Dataset>(xBlock, yBlock, zBlock, blockSize);
+        auto [xHalf, yHalf, zHalf] = makeLessDenseTemplate<T, Dataset>(2, xBlock, yBlock, zBlock, blockSize);
 
         size_t                    multi1D    = std::rint(cbrtNumPart / std::cbrt(blockSize));
         std::tuple<int, int, int> innerMulti = {16 * multi1D, 8 * multi1D, multi1D};
